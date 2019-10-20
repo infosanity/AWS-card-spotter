8aeb96dadbc0b703f6f6248d25814f3df3ef750d
Andrew Waite <andrew.waite@infosanity.co.uk>
2019-10-20 19:43:34 +0100

    AWS-card-spotter_LAMBDA ran through pylint

--- AWS-card-spotter/Lambda/AWS-card-spotter_LAMBDA.py
+++ AWS-card-spotter/Lambda/AWS-card-spotter_LAMBDA.py
@@ -2,7 +2,8 @@ import json
 import boto3
 
 def notify_sns(message):
-    client = boto3.client('sns',"eu-west-1")
+    """Send results to SNS for further processing."""
+    client = boto3.client('sns', "eu-west-1")
     response = client.publish(
         TargetArn="arn:aws:sns:eu-west-1:<<AWS ACCOUNT NUMBER>>:<<SNS TO PUBLISH TO>>",
         Message=json.dumps({'default': json.dumps(message)}),
@@ -10,11 +11,12 @@ def notify_sns(message):
     )
     print(response)
 
-def find_credit_card(bucket,image):
-    print("Analysing %s::%s" % (bucket,image))
+def find_credit_card(bucket, image):
+    """Uses AWS Rekognition to aid identification of credit cards in image."""
+    print("Analysing %s::%s" % (bucket, image))
     rekognition = boto3.client("rekognition")
     response = rekognition.detect_labels(
-        Image = {
+        Image={
             "S3Object":{
                 "Bucket": bucket,
                 "Name": image
@@ -26,14 +28,13 @@ def find_credit_card(bucket,image):
         if label['Name'] == "Credit Card":
             print("[%s] Credit Card Identified in %s with %i confidence" % (bucket, image, int(label['Confidence'])))
             notify_sns("[%s] Credit Card Identified in %s with %i confidence" % (bucket, image, int(label['Confidence'])))
-    return
 
 def lambda_handler(event, context):
+    """Ingress to Lambda function."""
     bucket = event['Records'][0]['s3']['bucket']['name']
     key = event['Records'][0]['s3']['object']['key']
-    find_credit_card(bucket,key)
+    find_credit_card(bucket, key)
     return {
         'statusCode': 200,
         'body': json.dumps('aws-card-spotter')
     }
-
